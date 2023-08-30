0490abf6c1a222a90c29c4f0c604c275f352554e
scroll <96938159+Umar12212@users.noreply.github.com>
2023-08-30 15:05:49 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 do{
-    var firstNUM = +prompt('Введите число')
+    let firstNUM = +prompt('Введите число')
 }while (isNaN(firstNUM) || firstNUM < 1)
 do{
-    var secondNUM = +prompt('Введите степень')
+    let secondNUM = +prompt('Введите степень')
 }while (isNaN(secondNUM) || secondNUM < 1) {
     alert((firstNUM ** secondNUM))
 }
